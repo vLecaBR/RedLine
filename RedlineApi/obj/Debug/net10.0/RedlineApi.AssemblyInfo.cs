c12a0a29cd92dc85517773f1dce3f3aa0daba4e2
vLecaBR <vitartasleca@gmail.com>
2026-07-10 17:19:43 -0300

    Update binaries and assembly info for RedlineApi

--- RedlineApi/obj/Debug/net10.0/RedlineApi.AssemblyInfo.cs
+++ RedlineApi/obj/Debug/net10.0/RedlineApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedlineApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590e32a0d9ef2318d6dbc8c76a0c3e3bd6c3cdf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RedlineApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RedlineApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
